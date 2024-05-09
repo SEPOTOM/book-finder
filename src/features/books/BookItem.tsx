850be07d4098c374bca3d360444325e31e0d9048
Artyom Pizin <artempetr2016@gmail.com>
2024-05-09 22:59:35 +0300

    feat: add image placeholder if there is no book cover

--- src/features/books/BookItem.tsx
+++ src/features/books/BookItem.tsx
@@ -4,21 +4,27 @@ import { useAppSelector } from '../../common/hooks';
 
 import { BookItemProps } from './types';
 
+import ImagePlaceholderSrc from '/img-placeholder.png';
+
 const BookItem = ({ bookId, searchQuery }: BookItemProps) => {
   const { selectSearchedBookById } = createSearchedBooksSelectors(searchQuery);
 
   const { cover_edition_key, title, author_name, publish_date } =
     useAppSelector((state) => selectSearchedBookById(state, bookId));
 
+  const noImageClassName = cover_edition_key
+    ? ''
+    : 'flex items-center h-full bg-gray rounded-lg';
+
   return (
     <li className="basis-1/3 px-4 py-2">
       <div className="flex items-start gap-x-5 h-full p-5 border-4 border-black rounded-lg shadow-lg">
-        <div className="shrink-0 w-44 max-h-72">
+        <div className={`shrink-0 w-44 max-h-72 ${noImageClassName}`}>
           <img
             src={
               cover_edition_key
                 ? `https://covers.openlibrary.org/b/olid/${cover_edition_key}-M.jpg`
-                : ''
+                : ImagePlaceholderSrc
             }
             alt={title || 'Unknown title'}
             className="max-w-full max-h-full object-contain rounded-lg"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: '#dcdcdc',
+      },
+    },
   },
   plugins: [],
 };
